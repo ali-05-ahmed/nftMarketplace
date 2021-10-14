faaeb5dd2e53209dbe9296291044f3b7ddfc767e
ali <extricateshareware@gmail.com>
2021-10-14 16:25:38 +0500

    marketplace sample added

--- scripts/upgrade_NftMarketplace.js
+++ scripts/upgrade_NftMarketplace.js
@@ -4,7 +4,7 @@ const { ethers, upgrades } = require('hardhat');
 async function main() {
     const NftMarketplaceV2 = await ethers.getContractFactory('NftMarketplaceV2');
     console.log('Upgrading NftMarketplace...');
-    await upgrades.upgradeProxy('0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', NftMarketplaceV2);
+    await upgrades.upgradeProxy('0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', NftMarketplaceV2, { initializer: 'initialize' });
     console.log('NftMarketplace upgraded');
 }
 
