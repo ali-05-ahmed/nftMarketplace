cdcb8b4df7d14ee776961d00222b62b57caa6adf
ali <extricateshareware@gmail.com>
2021-10-14 16:56:23 +0500

    NFT contract updated

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -12,17 +12,36 @@ contract Nft is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     address contractAddress;
 
+    struct NftDetails{
+        address[] owners;
+        uint256 creationTime;
+    }
+    mapping(uint256=>NftDetails) _NftDetails;
+
     constructor(address marketplaceAddress) ERC721("Metaverse", "METT") {
         contractAddress = marketplaceAddress;
+    }
+
+    function setNftDetails(uint256 _newItemId,address owner)private{
+        _NftDetails[_newItemId].owners.push(owner);
+        _NftDetails[_newItemId].creationTime=getTime();
     }
 
     function createToken(string memory tokenURI) public returns (uint) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
-
+        setNftDetails(newItemId,msg.sender)
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
         setApprovalForAll(contractAddress, true);
         return newItemId;
+    }
+    //returns the total number of Nfts minted from this contract
+    function totalNftminted() public returns(uint256){
+        return _tokenIds.current();
+    }
+
+    function getTime() private view returns(uint256){
+        return block.timestamp;
     }
 }
